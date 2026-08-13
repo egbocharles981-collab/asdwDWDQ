32dceb6743b93f1d53714d3f03ae773c53360139
Egbo Charles <egbocharles9@gmail.com>
2026-08-13 13:03:00 +0100

    Add src controllers shim for Render path variations

---
+++ src/controllers/candleController.js
@@ -0,0 +1,3 @@
+// Deployment shim for environments that place server files under `src/`
+// Forward exports to the project's root `controler.js` implementation.
+module.exports = require('../../controler');
